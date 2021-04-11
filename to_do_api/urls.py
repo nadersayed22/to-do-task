40156411d21d97f26bbea2fe857319ff4ad1dac7
nadersayed22 <nadersayedrabea22@gmail.com>
2021-04-11 11:35:13 +0200

    add url for viewset

---
+++ to_do_api/urls.py
@@ -0,0 +1,12 @@
+from .views import USerViewSet
+from rest_framework import routers
+from django.urls import path , include
+
+router=routers.DefaultRouter()
+router.register('profile',USerViewSet)
+
+
+urlpatterns = [
+
+    path("", include(router.urls))
+]
