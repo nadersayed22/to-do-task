b382af90ab6c807c66be432ddb8ee5dcd3de4a66
nadersayed22 <nadersayedrabea22@gmail.com>
2021-04-11 11:26:20 +0200

    add viweset to user

--- to_do_api/views.py
+++ to_do_api/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from serializer import UserSerializer
+from .models import USerProfile
 
+
+class USerViewSet(viewsets.ModelViewSet):
+    """
+    doing CRUD operation using viewset
+    """
+    serializer_class = UserSerializer
+    queryset = USerProfile.objects.all()
