083c84feb2bebcb403bd5f88c88f4767abbb57e0
nadersayed22 <nadersayedrabea22@gmail.com>
2021-04-11 07:55:57 +0200

    customize on user model

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'to_do_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- to_do_api/models.py
+++ to_do_api/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    """"
+    class required by django for managing our users from management command
+    """
+
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users Must Have EMail Address")
+
+        # create anew user object
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        # create new pass
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """"
+        create and save new superuser with given details
+        """
+
+        # override on create fun
+        user = self.create_user(email, password)
+
+        # make this user an admin
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class USerProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    a user profile in our system
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_superuser = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        """
+        What to show when we output an object as a string
+        """
+
+        return self.email
