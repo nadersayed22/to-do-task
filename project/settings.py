b2a7a724d417daf6aceaf160883df6c89e997c8e
nadersayed22 <nadersayedrabea22@gmail.com>
2021-04-13 10:11:08 +0200

    change names in serializer

--- project/settings.py
+++ project/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'to_do_react/build/static'),
+    os.path.join(BASE_DIR, 'templates/'),
 ]
 
 # Default primary key field type
--- to_do_api/admin.py
+++ to_do_api/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
-from .models import USerProfile, task
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+from .models import UserProfile, Task
+
 from django.contrib.auth.models import Group
 
+
 # Register your models here.
+@admin.ModelAdmin(UserProfile)
+class UserProfileModelAdmin(BaseUserAdmin):
+    add_form_template = 'core/add_user.html'
+    ordering = ['id']
+    list_display = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
 
 
-admin.site.register(USerProfile)
-admin.site.register(task)
+admin.site.register(Task)
