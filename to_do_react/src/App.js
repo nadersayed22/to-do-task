95b6bb1ca89b8a17174581c460e8f7a4a7a29e35
nadersayed22 <nadersayedrabea22@gmail.com>
2021-04-14 14:39:34 +0200

    add components and layout

--- to_do_react/src/App.js
+++ to_do_react/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+      <Router>
+        <Route exact path='/' component={Home} />
+        </Router>
+      </Layout>
+
+    );
+  }
 }
-
-export default App;
