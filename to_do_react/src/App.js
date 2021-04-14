d003e9adb84babfaca5edfd9a06e60e3dd7290c3
nadersayed22 <nadersayedrabea22@gmail.com>
2021-04-14 16:15:06 +0200

    adding logout & log in & register

--- to_do_react/src/App.js
+++ to_do_react/src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
+import React, { useState } from 'react';
+import './App.css';
+import Login from './components/login';
 
-export default class App extends Component {
-  static displayName = App.name;
+function App() {
 
-  render () {
-    return (
-      <Layout>
-      <Router>
-        <Route exact path='/' component={Home} />
-        </Router>
-      </Layout>
+  const [token, setToken] = useState('');
 
-    );
+  const userLogin = (tok) => {
+    setToken(tok);
   }
+
+  return (
+    <div className="App">
+      <Login userLogin={userLogin}/>
+    </div>
+  );
 }
+
+export default App;
